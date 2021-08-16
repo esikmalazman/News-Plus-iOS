3bfffc2e17d1a3b9f6a11a580237cac6ef299597
Ikmal Azman <opikmalazman@gmail.com>
2021-08-16 22:13:23 +0800

    declare result type enum to handle logic

---
+++ News+/Utilities/Result.swift
@@ -0,0 +1,16 @@
+//
+//  Result.swift
+//  News+
+//
+//  Created by Ikmal Azman on 15/08/2021.
+//
+
+import Foundation
+
+// For result type logic handling, associate
+// Result <> type, remove ambiguity of the optionals, value that represent either success/failure include associate value in each case
+enum Result<Success, Failure : Error> {
+    
+    case success(Success)
+    case failure(Failure)
+}

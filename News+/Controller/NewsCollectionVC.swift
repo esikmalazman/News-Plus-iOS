a642b3241acca44195ece1e2db3bb61704132fa6
Ikmal Azman <opikmalazman@gmail.com>
2021-08-16 22:17:38 +0800

    refactor news vc

--- News+/Controller/NewsCollectionVC.swift
+++ News+/Controller/NewsCollectionVC.swift
@@ -10,13 +10,9 @@ import SafariServices
 
 class NewsCollectionVC: UIViewController {
     
-    
-    
     let iso8601Formatter = ISO8601DateFormatter()
     
-    var newsManager = NewsManager()
-    var newsResults = [NewsModel]()
-    
+    var newsData = [News]()
     var dateFormatter : DateFormatter = {
         
         let formatter = DateFormatter()
@@ -28,33 +24,27 @@ class NewsCollectionVC: UIViewController {
     var pageNumber = 1
     var fetchMoreNews = false
     
-    
     @IBOutlet weak var newsSegments: UISegmentedControl!
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureVC()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         configureNavBar()
     }
     
-    func configureVC(){
-        
-        newsSegments.addUnderlineForSelectedSegment()
+    private func configureVC() {
         
         collectionView.showsVerticalScrollIndicator = false
         collectionView.delegate = self
         collectionView.dataSource = self
-        
-        newsManager.delegate = self
-        newsManager.fetchNews()
+        getNews()
     }
     
-    func configureNavBar(){
+    private func configureNavBar() {
         // Set navbar to be transparent
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         navigationController?.navigationBar.shadowImage = UIImage()
@@ -65,51 +55,74 @@ class NewsCollectionVC: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor(named: "NavBarColor") ?? .black]
     }
     
+    func getNews(topic : String = "World", page : Int = 1) {
+        
+        NetworkManager.shared.getNews(for: topic, at: page) { [weak self] result in
+            // you'll create a new local variable that will hold a strong reference to the outside weak self.
+            guard let strongSelf = self else { return }
+            
+            switch result {
+            
+            case .success(let news):
+                print("News in Success : \(news)")
+                strongSelf.updateUI(with: news)
+                
+            case .failure(let error):
+                // Debug custom error NError
+                print(error.rawValue)
+            }
+        }
+    }
+    
+    func updateUI(with news : [News]) {
+        
+        print("News Array : \(news.count)")
+        DispatchQueue.main.async {
+            self.newsData.append(contentsOf: news)
+            self.collectionView.reloadData()
+        }
+        if news.isEmpty {
+            print("There's no news available at the momment")
+        }
+    }
     
     
     @IBAction func newsDidSelect(_ sender: UISegmentedControl) {
         
         guard let segmentsTitle = sender.titleForSegment(at: newsSegments.selectedSegmentIndex) else {fatalError("There no segmented found") }
         
-        newsSegments.changeUnderlinePosition()
-        newsResults = []
-        newsManager.fetchNews(topics: segmentsTitle)
-        
+        newsData = []
+        getNews(topic: segmentsTitle)
     }
-    
-    
 }
 
 //MARK:- ColletionView DataSource
 
 extension NewsCollectionVC : UICollectionViewDataSource {
     
-    
     // Tell delegate how many cv need to show
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //return 2
-        return newsResults.count
+        return newsData.count
     }
     
     // Tell delegate content of cv
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let listOfNews = newsResults[indexPath.row]
+        let listOfNews = newsData[indexPath.row]
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CViewCell
         
         cell.title.text = listOfNews.title
-        cell.desc.text = listOfNews.decription
-        cell.source.text = listOfNews.sourcesName
-        
-        // Convert damn ISO8601 to other format, https://developer.apple.com/forums/thread/660878
-        if let date = iso8601Formatter.date(from: listOfNews.publishedAt){
-            let formattedString = dateFormatter.string(from: date)
-            cell.date.text = formattedString
-        }
-        
+        cell.desc.text = listOfNews.description
+        cell.source.text = listOfNews.source.name
         cell.newsImage.downloaded(from: listOfNews.image)
         cell.newsImage.contentMode = .scaleToFill
+        
+        // Convert damn ISO8601 to other format, https://developer.apple.com/forums/thread/660878
+        let date = iso8601Formatter.date(from: listOfNews.publishedAt)
+        let formattedString = dateFormatter.string(from: date ?? Date())
+        cell.date.text = formattedString
         
         return cell
     }
@@ -120,34 +133,15 @@ extension NewsCollectionVC : UICollectionViewDataSource {
 
 extension NewsCollectionVC : UICollectionViewDelegate {
     
-    
-    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedNews = newsResults[indexPath.row]
+        
+        let selectedNews = newsData[indexPath.row]
         // Provide VC to run url in app
         let SafariVC = SFSafariViewController(url: selectedNews.url)
-        // Present the app
+        // Present the news
         present(SafariVC, animated: true, completion: nil)
-        
     }
 }
-
-//MARK:- NewsManager Delegate
-
-extension NewsCollectionVC : NewsManagerDelegate {
-    
-    func didSendNewsData(_ newsManager: NewsManager, with news: [NewsModel]) {
-        
-        DispatchQueue.main.async{
-            
-            self.newsResults.append(contentsOf: news)
-            self.collectionView.reloadData()
-            print("Current Page Number : \(self.pageNumber) | News Result :\(self.newsResults.count)")
-            
-        }
-    }
-}
-
 
 //MARK:- Fetch more news methods
 
@@ -160,15 +154,12 @@ extension NewsCollectionVC {
         fetchMoreNews = true
         print("Begin Fetch More News")
         
-        
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
-            
-            print("Page number : \(self.pageNumber) | newResults : \(self.newsResults.count)")
+            print("Page number : \(self.pageNumber) | newResults : \(self.newsData.count)")
             self.fetchMoreNews = false
-            self.newsManager.fetchNews(page: self.pageNumber)
+            //self.newsManager.fetchNews(page: self.pageNumber)
         }
     }
-    
     
     // Tell delegate when user start scroll
     func scrollViewDidScroll(_ scrollView: UIScrollView) {

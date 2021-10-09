35f6b0505f108de9acda8f7728c05d83d1a843ef
Ikmal Azman <opikmalazman@gmail.com>
2021-10-10 01:57:28 +0800

    remove unnecessary code in a codebase

--- News+/Controller/NewsCollectionVC.swift
+++ News+/Controller/NewsCollectionVC.swift
@@ -13,13 +13,6 @@ class NewsCollectionVC: UIViewController {
     let iso8601Formatter = ISO8601DateFormatter()
     
     var newsData = [News]()
-    var dateFormatter : DateFormatter = {
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd-MM-yyyy"
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-        return formatter
-    }()
     
     var pageNumber = 1
     var fetchMoreNews = false
@@ -55,25 +48,18 @@ class NewsCollectionVC: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor(named: "NavBarColor") ?? .black]
     }
     
-    func getNews(topic : String = "World", page : Int = 1) {
+    func getNews(topic : String = "World") {
         
-        NetworkManager.shared.getNews(for: topic, at: page) { [weak self] result in
-            // you'll create a new local variable that will hold a strong reference to the outside weak self.
-            guard let strongSelf = self else { return }
-            
+        NetworkManager.shared.fetchGenericData(topic: topic) { (result : Result<NewsResponse, NError>) in
             switch result {
-            
             case .success(let news):
-                print("News in Success : \(news)")
-                strongSelf.updateUI(with: news)
-                
-            case .failure(let error):
-                // Debug custom error NError
-                print(error.rawValue)
+                self.updateUI(with: news.articles)
+            case .failure(let failure):
+                print(failure.rawValue)
             }
         }
     }
-    
+
     func updateUI(with news : [News]) {
         
         print("News Array : \(news.count)")
@@ -113,24 +99,14 @@ extension NewsCollectionVC : UICollectionViewDataSource {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CViewCell
         
-        cell.title.text = listOfNews.title
-        cell.desc.text = listOfNews.description
-        cell.source.text = listOfNews.source.name
-        cell.newsImage.downloaded(from: listOfNews.image)
-        cell.newsImage.contentMode = .scaleToFill
-        
-        // Convert damn ISO8601 to other format, https://developer.apple.com/forums/thread/660878
-        let date = iso8601Formatter.date(from: listOfNews.publishedAt)
-        let formattedString = dateFormatter.string(from: date ?? Date())
-        cell.date.text = formattedString
-        
+        cell.configureCell(data: listOfNews)
+
         return cell
     }
     
 }
 
 //MARK:- CollectionView Delegate
-
 extension NewsCollectionVC : UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -146,34 +122,4 @@ extension NewsCollectionVC : UICollectionViewDelegate {
 //MARK:- Fetch more news methods
 
 // TODO : Add pagination/infinite scroll in future
-extension NewsCollectionVC {
-    
-    // Function to fetch more news
-    func fetchOtherNews(){
-        
-        fetchMoreNews = true
-        print("Begin Fetch More News")
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
-            print("Page number : \(self.pageNumber) | newResults : \(self.newsData.count)")
-            self.fetchMoreNews = false
-            //self.newsManager.fetchNews(page: self.pageNumber)
-        }
-    }
-    
-    // Tell delegate when user start scroll
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        let offSetY = scrollView.contentOffset.y
-        let contentHeight = scrollView.contentSize.height
-        
-        if offSetY > contentHeight - scrollView.frame.height * 2 {
-            
-            if !fetchMoreNews{
-                pageNumber += 1
-                //fetchOtherNews()
-                //collectionView.reloadData()
-            }
-        }
-    }
-}
+extension NewsCollectionVC {}

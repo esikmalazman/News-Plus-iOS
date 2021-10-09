471b0d31a71eda92d2d1440f5c85f6da2d7c9028
Ikmal Azman <opikmalazman@gmail.com>
2021-10-10 01:42:56 +0800

    adding new case for decode error

--- News+/Utilities/NError.swift
+++ News+/Utilities/NError.swift
@@ -11,7 +11,8 @@ import Foundation
 enum NError : String, Error {
     
     case invalidTopic = "The topic requested is invalid request. Please try again"
-    case unableToComplete = "Unable to complete your request. Please check your internet connection"
     case invalidResponse = "Invalid response from the server. Please try again"
     case invalidData = "The data received from server was invalid. Please Try Again"
+    case unableToComplete = "Unable to complete your request. Please check your internet connection"
+    case unableToDecode = "Unable to decode the object from data. Please check you configuration"
 }

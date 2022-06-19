6c78e4c8fa5f387d029b98b091836c63fe112933
Ikmal Azman <opikmalazman@gmail.com>
2022-06-19 08:53:47 +0800

    add test helpers for collectionview

--- News+Tests/TestHelpers.swift
+++ News+Tests/TestHelpers.swift
@@ -5,4 +5,21 @@
 //  Created by Ikmal Azman on 19/06/2022.
 //
 
-import Foundation
+import UIKit
+
+//MARK: - CollectionView Test Helpers
+
+func numberOfItemsInSection(_ collectionView: UICollectionView, section : Int = 0)-> Int? {
+    return collectionView.dataSource?
+        .collectionView(
+            collectionView,
+            numberOfItemsInSection: section)
+}
+
+func cellForItemAt(_ collectionView : UICollectionView,
+                   row : Int,
+                   section : Int = 0) -> UICollectionViewCell? {
+    let indexPath  = IndexPath(row: row, section: section)
+    let cell = collectionView.dataSource?.collectionView(collectionView, cellForItemAt: indexPath)
+    return cell
+}
